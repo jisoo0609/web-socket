6f67cec7221f9ae3daa5f49edbcab943d14c2159
JISOO <jspark@mobilecen.co.kr>
2024-10-30 17:18:05 +0900

    create: init

---
+++ src/main/java/com/example/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@com.example.chat.SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
