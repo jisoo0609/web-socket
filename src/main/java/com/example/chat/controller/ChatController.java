87884d278825ee0c713a5b0d28bebfce6249c738
JISOO <jspark@mobilecen.co.kr>
2024-11-01 13:17:22 +0900

    feat: controller mapping

---
+++ src/main/java/com/example/chat/controller/ChatController.java
@@ -0,0 +1,24 @@
+package com.example.chat.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+    @RequestMapping("/lobby")
+    public String lobby() {
+        log.info("=====enter lobby=====");
+        return "lobby";
+    }
+
+    @RequestMapping("/enter")
+    public String enter() {
+        log.info("=====enter=====");
+        return "chat";
+    }
+
+}
