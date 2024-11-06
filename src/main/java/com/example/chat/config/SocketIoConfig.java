c0bcd0aa03c459a7a3ece40cc88c3f9f0898d841
JISOO <jisu20000609@gmail.com>
2024-11-06 17:25:27 +0900

    feat: socket.io

---
+++ src/main/java/com/example/chat/config/SocketIoConfig.java
@@ -0,0 +1,49 @@
+package com.example.chat.config;
+
+
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SocketIoConfig {
+    @Value("${socketio.server.hostname}")
+    private String hostname;
+
+    @Value("${socketio.server.port}")
+    private int port;
+
+    // create socketIO Server
+    private SocketIOServer server;
+
+    @Bean
+    public SocketIOServer socketIoServer() {
+        Configuration config = new Configuration();
+
+        config.setHostname(hostname);
+        config.setPort(port);
+
+        server = new SocketIOServer(config);
+        log.info("=====>server start!");
+        server.start();
+
+        server.addConnectListener(client -> log.info("Client connected: {}", client.getSessionId()));
+        server.addDisconnectListener(client -> log.info("Client disconnected: {}", client.getSessionId()));
+
+        return server;
+    }
+
+    @PreDestroy
+    public void stopSocketServer() {
+        this.server.stop();
+    }
+}
--- src/main/java/com/example/chat/model/ChatMessage.java
+++ src/main/java/com/example/chat/model/ChatMessage.java
@@ -4,6 +4,19 @@ import lombok.Data;
 
 @Data
 public class ChatMessage {
+    private MessageType type;
     private String username;
     private String message;
+
+    public ChatMessage() {
+
+    }
+
+    public ChatMessage(MessageType type, String username, String message) {
+        this.type = type;
+        this.username = username;
+        this.message = message;
+    }
 }
+
+
